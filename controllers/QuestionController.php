405229a42153b69885073251c5b8244f7d8864ac
jojeringnax <hollow718@gmail.com>
2017-06-23 16:51:54 +0300

    Доработка кнопки "Далее" в вопросах

--- controllers/QuestionController.php
+++ controllers/QuestionController.php
@@ -20,15 +20,11 @@ class QuestionController extends Controller
 
     public function actionView()
     {
-
-        if(Yii::$app->request->isAjax) {
-
             $id = Yii::$app->request->get('id');
             $question = Question::findOne($id);
             if(empty($question)) {throw new \yii\web\HttpException(404, 'Такой страницы, наверное, нет');}
                 return $this->renderPartial('view', [
                     'question' => $question,
                 ]);
-        }
     }
 }
