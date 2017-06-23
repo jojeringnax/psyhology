20c366f9e6fa0e9a856abd6f1fd42f5fd42207ab
jojeringnax <hollow718@gmail.com>
2017-06-23 20:50:11 +0300

    ДОработка кнопки "Далее на вопросах!"

--- views/question/view.php
+++ views/question/view.php
@@ -1,5 +1,5 @@
 <div class="quetModalWrapper">
     <div class="modal-header" style="text-align: left;"><b><i><?= $question->questionBody ?></i></b></div>
-    <span style="text: left;"><b>Ответ:</b></span>
-    <div class="modal-body"><br /><?= $question->answerBody ?></div>
+    <div style="text-align: left;"><span style="text: left;"><b>ОТВЕТ:</b></span></div>
+    <div class="modal-body"><?= $question->answerBody ?></div>
 </div>
