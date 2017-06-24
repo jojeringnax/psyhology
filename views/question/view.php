a8a15cb5e288684253973b41681cb2ced7f7c7d3
jojeringnax <hollow718@gmail.com>
2017-06-24 09:54:28 +0300

    ДОработка кнопки "Далее на вопросах!"

--- views/question/view.php
+++ views/question/view.php
@@ -1,3 +1,4 @@
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 <div class="questModalWrapper">
     <div class="modal-header" style="text-align: left;"><b><i><?= $question->questionBody ?></i></b></div>
     <span><b>ОТВЕТ:</b></span>
